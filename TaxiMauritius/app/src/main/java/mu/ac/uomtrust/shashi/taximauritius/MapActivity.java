552b7fbc8cef29024876486f18743135865722a2
ashwin <ash.the.reaper@gmail.com>
2017-05-28 14:23:15 +0400

    google map in real time

--- TaxiMauritius/app/src/main/java/mu/ac/uomtrust/shashi/taximauritius/MapActivity.java
+++ TaxiMauritius/app/src/main/java/mu/ac/uomtrust/shashi/taximauritius/MapActivity.java
@@ -1,6 +1,12 @@
 package mu.ac.uomtrust.shashi.taximauritius;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +20,15 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 /**
  * Created by Ashwin on 28-May-17.
  */
 
-public class MapActivity extends Fragment {
+public class MapActivity extends Fragment implements LocationListener{
 
     private MapView mMapView;
     private GoogleMap googleMap;
@@ -43,18 +53,8 @@ public class MapActivity extends Fragment {
             @Override
             public void onMapLoaded() {
                 googleMap = mMapView.getMap();
-
-                // For showing a move to my location button
                 googleMap.setMyLocationEnabled(true);
-
-                // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-20.241749, 57.489728);
-                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
+                getLastKnownLocation();
             }
         });
 
@@ -85,4 +85,69 @@ public class MapActivity extends Fragment {
         mMapView.onLowMemory();
     }
 
+    private void getLastKnownLocation(){
+        try {
+            LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateLocation(Location location) {
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        //String longitude = "Longitude: " + loc.getLongitude();
+        //String latitude = "Latitude: " + loc.getLatitude();
+        String cityName = null;
+
+        Geocoder gcd = new Geocoder(getActivity(), Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = gcd.getFromLocation(location.getLatitude(),location.getLongitude(), 1);
+            if (addresses.size() > 0) {
+                cityName = addresses.get(0).getLocality();
+            }
+
+            // For dropping a marker at a point on the Map
+            LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+            googleMap.addMarker(new MarkerOptions().position(currentPosition).title("Marker Title").snippet("Marker Description"));
+
+            // For zooming automatically to the location of the marker
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(currentPosition).zoom(12).build();
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        updateLocation(location);
+
+/*        if ( ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },255);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);*/
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
